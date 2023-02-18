b191f8e2886ed2114a6c044820d9e77bd7ae5cf0
Julio C. MW11 <sysa.mail@gmail.com>
2023-02-18 00:32:28 -0600

    se agrega page resumen

--- src/components/pages/Resumen.js
+++ src/components/pages/Resumen.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { Fragment }  from "react";
 
 export default function Resumen () {
     return (
-        <h1>Resumen</h1>
+        <Fragment>
+            <p className="blockquote mt-3">Resumen</p>
+            <figure>
+            <blockquote className="blockquote">
+                <h5 className="blockquote mt-3">Competencias Font-end:</h5>
+                <ul>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                    <li>JQuery</li>
+                    <li>Diseño responsivo</li>
+                    <li>React</li>
+                    <li>Bootstrap</li>
+                </ul>
+
+                <h5 className="blockquote mt-3">Competencias Back-end:</h5>
+                <ul>
+                <li>APIs</li>
+                <li>Node</li>
+                <li>Express</li>
+                <li>MySQL, Sequelize</li>
+                <li>MONGO DB, Graphql</li>
+                </ul>
+            </blockquote>
+            </figure>
+
+    </Fragment>
+
     );
 }

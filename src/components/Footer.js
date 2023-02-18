988f72798309818f1524b0d796742176c060c6f0
Julio C. MW11 <sysa.mail@gmail.com>
2023-02-17 23:57:46 -0600

    se agrega informacion de bio page, se corrige footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,15 +3,15 @@ import React from "react";
 export default function Footer(){
     return(
         <footer className="footer fixed-bottom container text-center col-md-2">
-            <a className="ms-4 me-4" href="https://github.com/JulioCesarDelAngel" target="_blank" rel="noreferrer">
+            <a className="ms-2 me-2" href="https://github.com/JulioCesarDelAngel" target="_blank" rel="noreferrer">
                 <i className="bi bi-github" style={{fontSize:"3rem",color: "white"}}></i>
             </a>
 
-            <a className="ms-4 me-4" href="https://www.linkedin.com/in/JuliusMathison" target="_blank" rel="noreferrer">
+            <a className="ms-2 me-2" href="https://www.linkedin.com/in/JuliusMathison" target="_blank" rel="noreferrer">
                 <i className="bi bi-linkedin" style={{fontSize:"3rem",color: "white"}}></i>
             </a>
 
-            <a className="ms-4 me-4" href="https://stackoverflow.com/users/19882522" target="_blank" rel="noreferrer">
+            <a className="ms-2 me-2" href="https://stackoverflow.com/users/19882522" target="_blank" rel="noreferrer">
                 <i className="bi bi-stack-overflow" style={{fontSize:"3rem",color: "white"}}></i>
             </a>
         </footer>
--- src/components/pages/Bio.js
+++ src/components/pages/Bio.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { Fragment } from "react";
+import profileImage from '../../assets/images/myAvatar_bw.png'
 
 export default function Bio () {
     return (
-        <h1>Bio</h1>
+        <Fragment>
+            <p className="blockquote mt-3">Acerca de mi</p>
+            <img src={profileImage} className="rounded-circle" style={{with:"100px",height:"100px"}}  
+            borderRadius="50%" alt="Cinque Terre"></img>
+                <figure class="text-center">
+                <blockquote className="blockquote mt-3">
+                    <p className="text-start">Soy un ingeniero de software, apasionado por la tecnología y la mejora continua.
+                        Empleado en una de las grandes consultorías de San Pedro Garza García, México. 
+                        Participado en diferentes proyectos de diversas industrias en todos los roles del ciclo de desarrollo de software.
+                        </p>
+                        <p className="text-start">Como FreeLancer realizó consultoría de desarrollo de software a la medida, diseño, análisis, implementación y mantenimiento.</p>
+                        <p className="text-start">¿Tienes una idea?</p>
+                        <p className="text-start">¡¡Hagámosla!!</p>                        
+                </blockquote>
+                </figure>
+
+        </Fragment>
     );
 }

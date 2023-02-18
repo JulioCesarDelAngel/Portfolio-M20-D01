a4dbff85ef32b3353003a4f5a1194ee8ecb927fe
Julio C. MW11 <sysa.mail@gmail.com>
2023-02-17 22:16:46 -0600

    se agrega margen a footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,16 +2,16 @@ import React from "react";
 
 export default function Footer(){
     return(
-        <footer className="footer fixed-bottom container ">
-            <a href="https://github.com/JulioCesarDelAngel" target="_blank" rel="noreferrer">
+        <footer className="footer fixed-bottom container text-center col-md-2">
+            <a className="ms-4 me-4" href="https://github.com/JulioCesarDelAngel" target="_blank" rel="noreferrer">
                 <i className="bi bi-github" style={{fontSize:"3rem",color: "white"}}></i>
             </a>
 
-            <a href="https://www.linkedin.com/in/JuliusMathison" target="_blank" rel="noreferrer">
+            <a className="ms-4 me-4" href="https://www.linkedin.com/in/JuliusMathison" target="_blank" rel="noreferrer">
                 <i className="bi bi-linkedin" style={{fontSize:"3rem",color: "white"}}></i>
             </a>
 
-            <a href="https://stackoverflow.com/users/19882522" target="_blank" rel="noreferrer">
+            <a className="ms-4 me-4" href="https://stackoverflow.com/users/19882522" target="_blank" rel="noreferrer">
                 <i className="bi bi-stack-overflow" style={{fontSize:"3rem",color: "white"}}></i>
             </a>
         </footer>

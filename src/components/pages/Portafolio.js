bc21ea756607d6358701b5f8f3b88a337ab52da9
Julio C. MW11 <sysa.mail@gmail.com>
2023-02-19 18:50:21 -0600

    se agrega portafolio

--- src/components/pages/Portafolio.js
+++ src/components/pages/Portafolio.js
@@ -1,7 +1,33 @@
 import React from "react";
+import { projects } from "../../data/projects"
 
 export default function Portafolio () {
     return (
-        <h1>Portafolio</h1>
+        <section id="projects">
+            <h1 className="text-center"><i class="bi bi-code-slash"></i></h1>
+            <h3 className="text-center">Portafolio y colaboraciones</h3>
+            <div class="row row-cols-1 row-cols-md-2 g-4">
+                {
+                    projects.map((project)=>(
+                        <div className="col">
+                            <div className="card h-100">
+                            <div class="card-header bg-transparent border-info">
+                                <a className="card-title" href={project.link} target="_blank" rel="noreferrer">
+                                    {project.titulo}
+                                </a>
+                            </div>                            
+
+                            <img src={project.imagen} className="card-img-top h-100" alt={project.titulo}/>
+                            <div class="card-footer bg-transparent border-info">
+                                <a className="card-text" href={project.git} target="_blank" rel="noreferrer">
+                                <i className="bi bi-github" style={{fontSize:"2rem",color: "white"}}></i>
+                                </a>                                
+                            </div>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+        </section>
     );
 }
---
+++ src/data/projects.js
@@ -0,0 +1,32 @@
+export const projects =[
+    {
+        titulo:"Tech-Blog",
+        subtitulo:"Blog CMS FullStack",
+        imagen:"https://raw.githubusercontent.com/JulioCesarDelAngel/BLOG-CMS-M14-D01/main/assets/images/BLOG-CMS-M14-D01.png",
+        git:"https://github.com/JulioCesarDelAngel/BLOG-CMS-M14-D01",
+        link:"https://dry-oasis-39717.herokuapp.com/"
+    },
+    {
+        titulo:"SPA-PWA Editor",
+        subtitulo:"Editor de texto en Aplicación SPA",
+        imagen:"https://raw.githubusercontent.com/JulioCesarDelAngel/EDITOR-SPA-PWA-M19-D01/main/assets/images/EDITOR-SPA-PWA-M19-D01.png",
+        git:"https://github.com/JulioCesarDelAngel/EDITOR-SPA-PWA-M19-D01",
+        link:"https://mighty-garden-98776.herokuapp.com"
+    },
+    {
+        titulo:"Synesthesia",
+        subtitulo:"Editor de texto en Aplicación SPA",
+        imagen:"https://github.com/djonguitud/synesthesia-project/raw/main/assets/media/Synesthesia.gif",
+        git:"https://github.com/djonguitud/synesthesia-project",
+        link:"https://djonguitud.github.io/synesthesia-project/"
+    },
+    {
+        titulo:"TechInventoryApp",
+        subtitulo:"Sistema de contrl de inventario de computo",
+        imagen:"https://github.com/carlosgortiz/tech-inventory-web-app/raw/main/assets/images/tech%20-%20inventory.gif",
+        git:"https://github.com/carlosgortiz/tech-inventory-web-app",
+        link:"https://github.com/carlosgortiz/tech-inventory-web-app"
+    }
+
+
+]

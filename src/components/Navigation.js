95c3c842195ff5b2259a6370572670faa871f561
Julio C. MW11 <sysa.mail@gmail.com>
2023-02-19 19:28:47 -0600

    se agregan datos de contacto

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,7 +4,7 @@ export default function Navigation({currentPage, handlePageChange}){
 
 <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#x">Julio Cesar Del Angel</a>
+    <a className="navbar-brand" href="/">Julio Cesar Del Angel</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
--- src/components/pages/Contacto.js
+++ src/components/pages/Contacto.js
@@ -2,6 +2,18 @@ import React from "react";
 
 export default function Contacto () {
     return (
-        <h1>Contacto</h1>
+        <div className="container text-center align-middle">
+
+            <legend>Contacto</legend>
+            <a className="ms-2 me-2" href="mailto:sysa.mail@gmail.com" target="_blank" rel="noreferrer">
+                <i className="bi bi-envelope align-middle" style={{fontSize:"3rem",color: "white"}}></i>
+                sysa.mail@gmail.com
+            </a>
+            <a className="ms-2 me-2" href="tel:+528112556606" target="_blank" rel="noreferrer">
+                <i className="bi bi-phone align-middle" style={{fontSize:"3rem",color: "white"}}></i>
+                +528112556606
+            </a>
+        </div>
+        
     );
 }
